b902b0ace2db84a100c87a0024155fc9e14f7198
alastifer <grishkevichaaa@gmail.com>
2017-05-09 13:06:00 +0300

    DAO interface

---
+++ src/main/java/com/project/coffee/shop/dao/DAO.java
@@ -0,0 +1,15 @@
+package com.project.coffee.shop.dao;
+
+import com.project.coffee.shop.dao.exception.DAOException;
+import com.project.coffee.shop.dao.exception.NoSuchEntityException;
+import com.project.coffee.shop.entity.Coffee;
+
+import java.util.List;
+
+public interface DAO {
+
+    Coffee getCoffeeById(Integer id) throws DAOException, NoSuchEntityException;
+
+    List<Coffee> getAllCoffee() throws DAOException;
+
+}
