c3f42ebc752a64cb1cab229aefebff5f35b79414
alastifer <grishkevichaaa@gmail.com>
2017-05-12 20:25:28 +0300

    add set and get methods

--- src/main/java/com/project/coffee/shop/dao/DAO.java
+++ src/main/java/com/project/coffee/shop/dao/DAO.java
@@ -3,6 +3,8 @@ package com.project.coffee.shop.dao;
 import com.project.coffee.shop.dao.exception.DAOException;
 import com.project.coffee.shop.dao.exception.NoSuchEntityException;
 import com.project.coffee.shop.entity.Coffee;
+import com.project.coffee.shop.entity.Order;
+import com.project.coffee.shop.entity.OrderElement;
 
 import java.util.List;
 
@@ -11,5 +13,11 @@ public interface DAO {
     Coffee getCoffeeById(Integer id) throws DAOException, NoSuchEntityException;
 
     List<Coffee> getAllCoffee() throws DAOException;
+
+    void setOrderElement(OrderElement orderElement) throws DAOException;
+
+    void setOrder(Order order) throws DAOException;
+
+    Integer getNextOrderId() throws DAOException;
 
 }
