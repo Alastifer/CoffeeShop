3c9340285f125a063573bd58d3076fcaf145e540
alastifer <grishkevichaaa@gmail.com>
2017-05-12 20:24:57 +0300

    add method for set order and orderElement, get for order id

--- src/main/java/com/project/coffee/shop/dao/CoffeeDAO.java
+++ src/main/java/com/project/coffee/shop/dao/CoffeeDAO.java
@@ -3,23 +3,34 @@ package com.project.coffee.shop.dao;
 import com.project.coffee.shop.dao.exception.DAOException;
 import com.project.coffee.shop.dao.exception.NoSuchEntityException;
 import com.project.coffee.shop.entity.Coffee;
+import com.project.coffee.shop.entity.Order;
+import com.project.coffee.shop.entity.OrderElement;
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class CoffeeDAO implements DAO {
 
     private final static String QUERY_SELECT_ALL = "FROM Coffee";
 
-    private final static SessionFactory sessionHibernate = new Configuration().configure().buildSessionFactory();
+    private final static String QUERY_SELECT_ORDER_ID = "SELECT order_id FROM Configurations";
+
+    private final static String QUERY_INCREMENT_ORDER_ID = "UPDATE Configurations SET order_id=";
+
+    private final static SessionFactory SESSION_HIBERNATE = new Configuration().configure().buildSessionFactory();
 
     @Override
     public Coffee getCoffeeById(Integer id) throws DAOException, NoSuchEntityException {
-        try (Session session = sessionHibernate.openSession()) {
+        Transaction tx = null;
+        try (Session session = SESSION_HIBERNATE.openSession()) {
+            tx = session.beginTransaction();
             Coffee coffee = session.get(Coffee.class, id);
+            tx.commit();
 
             if (coffee == null) {
                 throw new NoSuchEntityException("No coffee with id = " + id);
@@ -27,16 +38,77 @@ public class CoffeeDAO implements DAO {
 
             return coffee;
         } catch (HibernateException e) {
-            throw new DAOException("Problem with database", e);
+            if (tx != null) {
+                tx.rollback();
+            }
+            throw new DAOException("Problem with database : " + e.getMessage() + '\n' + Arrays.toString(e.getStackTrace()), e);
         }
     }
 
     @Override
     public List<Coffee> getAllCoffee() throws DAOException {
-        try (Session session = sessionHibernate.openSession()) {
-            return session.createQuery(QUERY_SELECT_ALL).list();
+        Transaction tx = null;
+        List<Coffee> coffeeList = null;
+        try (Session session = SESSION_HIBERNATE.openSession()) {
+            tx = session.beginTransaction();
+            coffeeList = session.createQuery(QUERY_SELECT_ALL).list();
+            tx.commit();
+
+            return coffeeList;
         } catch (HibernateException e) {
-            throw new DAOException("Problem with database", e);
+            if (tx != null) {
+                tx.rollback();
+            }
+            throw new DAOException("Problem with database : " + e.getMessage() + '\n' + Arrays.toString(e.getStackTrace()), e);
+        }
+    }
+
+    @Override
+    public void setOrderElement(OrderElement orderElement) throws DAOException {
+        Transaction tx = null;
+        try (Session session = SESSION_HIBERNATE.openSession()) {
+            tx = session.beginTransaction();
+            session.save(orderElement);
+            tx.commit();
+        } catch (HibernateException e) {
+            if (tx != null) {
+                tx.rollback();
+            }
+            throw new DAOException("Problem with database : " + e.getMessage() + '\n' + Arrays.toString(e.getStackTrace()), e);
+        }
+    }
+
+    @Override
+    public void setOrder(Order order) throws DAOException {
+        Transaction tx = null;
+        try (Session session = SESSION_HIBERNATE.openSession()) {
+            tx = session.beginTransaction();
+            session.save(order);
+            tx.commit();
+        } catch (HibernateException e) {
+            if (tx != null) {
+                tx.rollback();
+            }
+            throw new DAOException("Problem with database : " + e.getMessage() + '\n' + Arrays.toString(e.getStackTrace()), e);
+        }
+    }
+
+    @Override
+    public Integer getNextOrderId() throws DAOException {
+        Transaction tx = null;
+        try (Session session = SESSION_HIBERNATE.openSession()) {
+            tx = session.beginTransaction();
+
+            Integer nextId = (Integer) session.createNativeQuery(QUERY_SELECT_ORDER_ID).getSingleResult();
+            session.createNativeQuery(QUERY_INCREMENT_ORDER_ID + (nextId + 1)).executeUpdate();
+
+            tx.commit();
+            return nextId;
+        } catch (HibernateException e) {
+            if (tx != null) {
+                tx.rollback();
+            }
+            throw new DAOException("Problem with database : " + e.getMessage() + '\n' + Arrays.toString(e.getStackTrace()), e);
         }
     }
 
