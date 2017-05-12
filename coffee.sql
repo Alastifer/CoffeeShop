9fcc13c080f759c26a6b28f24c59f8a0dbf7f990
alastifer <grishkevichaaa@gmail.com>
2017-05-12 20:33:24 +0300

    sql for create all tables

---
+++ coffee.sql
@@ -0,0 +1,30 @@
+CREATE TABLE CoffeeGrades (
+	id INT AUTO_INCREMENT,
+	title VARCHAR(100) NOT NULL,
+	cost INT NOT NULL,
+	PRIMARY KEY (id)
+);
+	
+
+CREATE TABLE OrderElements (
+	id INT AUTO_INCREMENT,
+	order_id INT NOT NULL,
+	coffee_id INT NOT NULL,
+	amount_of_coffee INT NOT NULL,
+	total_cost INT NOT NULL,
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE Configurations (
+	order_id INT
+);
+
+CREATE TABLE Orders (
+	id INT NOT NULL,
+	customer_full_name VARCHAR(100) NOT NULL,
+	customer_address VARCHAR(100) NOT NULL,
+	total_cost INT NOT NULL,
+	PRIMARY KEY (id)
+);
+
+INSERT INTO Configurations VALUE (1);
