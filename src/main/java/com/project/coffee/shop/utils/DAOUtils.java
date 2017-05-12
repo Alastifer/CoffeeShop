c55e62a186bde191e02f5becc8d5507f4e1cf85b
alastifer <grishkevichaaa@gmail.com>
2017-05-12 20:27:52 +0300

    add method for work with dao

--- src/main/java/com/project/coffee/shop/utils/DAOUtils.java
+++ src/main/java/com/project/coffee/shop/utils/DAOUtils.java
@@ -4,6 +4,8 @@ import com.project.coffee.shop.dao.CoffeeDAO;
 import com.project.coffee.shop.dao.exception.DAOException;
 import com.project.coffee.shop.dao.exception.NoSuchEntityException;
 import com.project.coffee.shop.entity.Coffee;
+import com.project.coffee.shop.entity.Order;
+import com.project.coffee.shop.entity.OrderElement;
 
 import java.io.IOException;
 import java.util.List;
@@ -23,6 +25,30 @@ public class DAOUtils {
     public static List<Coffee> getAllCoffee() throws IOException {
         try {
             return DAO.getAllCoffee();
+        } catch (DAOException e) {
+            throw new IOException(e.getMessage(), e);
+        }
+    }
+
+    public static void setOrderElement(OrderElement orderElement) throws IOException {
+        try {
+            DAO.setOrderElement(orderElement);
+        } catch (DAOException e) {
+            throw new IOException(e.getMessage(), e);
+        }
+    }
+
+    public static void setOrder(Order order) throws IOException {
+        try {
+            DAO.setOrder(order);
+        } catch (DAOException e) {
+            throw new IOException(e.getMessage(), e);
+        }
+    }
+
+    public static Integer getNextOrderId() throws IOException {
+        try {
+            return DAO.getNextOrderId();
         } catch (DAOException e) {
             throw new IOException(e.getMessage(), e);
         }
