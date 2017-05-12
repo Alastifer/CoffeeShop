579154ade9b6f0eb6630608e1559e05701b85755
alastifer <grishkevichaaa@gmail.com>
2017-05-12 20:26:55 +0300

    add field for work with hibernate, rename some fields

--- src/main/java/com/project/coffee/shop/entity/OrderElement.java
+++ src/main/java/com/project/coffee/shop/entity/OrderElement.java
@@ -2,28 +2,64 @@ package com.project.coffee.shop.entity;
 
 public class OrderElement {
 
+    private int id;
+
+    private int orderId;
+
     private Coffee coffee;
 
-    private int amount;
+    private int amountOfCoffee;
 
-    private int total;
+    private int totalCost;
 
-    public OrderElement(Coffee coffee, int amount) {
+    public OrderElement() {
+    }
+
+    public OrderElement(int orderId, Coffee coffee, int amountOfCoffee) {
+        this.orderId = orderId;
         this.coffee = coffee;
-        this.amount = amount;
-        this.total = coffee.getCost() * amount - (amount / 3 * coffee.getCost());
+        this.amountOfCoffee = amountOfCoffee;
+        this.totalCost = coffee.getCost() * amountOfCoffee - (amountOfCoffee / 3 * coffee.getCost());
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getOrderId() {
+        return orderId;
     }
 
     public Coffee getCoffee() {
         return coffee;
     }
 
-    public int getAmount() {
-        return amount;
+    public int getAmountOfCoffee() {
+        return amountOfCoffee;
     }
 
-    public int getTotal() {
-        return total;
+    public int getTotalCost() {
+        return totalCost;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setOrderId(int orderId) {
+        this.orderId = orderId;
+    }
+
+    public void setCoffee(Coffee coffee) {
+        this.coffee = coffee;
+    }
+
+    public void setAmountOfCoffee(int amountOfCoffee) {
+        this.amountOfCoffee = amountOfCoffee;
+    }
+
+    public void setTotalCost(int totalCost) {
+        this.totalCost = totalCost;
     }
 
 }
