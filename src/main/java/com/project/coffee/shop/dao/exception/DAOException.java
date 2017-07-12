98cf91663e46537ca49fad3bd0f9e5e19534f43a
alastifer <grishkevichaaa@gmail.com>
2017-07-12 11:56:42 +0300

    add comments

--- src/main/java/com/project/coffee/shop/dao/exception/DAOException.java
+++ src/main/java/com/project/coffee/shop/dao/exception/DAOException.java
@@ -1,11 +1,25 @@
 package com.project.coffee.shop.dao.exception;
 
+/**
+ * Main exception for DAO.
+ */
 public class DAOException extends Exception {
 
+    /**
+     * Constructs exception with message.
+     *
+     * @param message exception's message
+     */
     public DAOException(String message) {
         super(message);
     }
 
+    /**
+     * Constructs exception with message and cause.
+     *
+     * @param message exception's message
+     * @param cause cause of exception
+     */
     public DAOException(String message, Throwable cause) {
         super(message, cause);
     }
--- src/main/java/com/project/coffee/shop/dao/exception/ProblemWithDatabaseException.java
+++ src/main/java/com/project/coffee/shop/dao/exception/ProblemWithDatabaseException.java
@@ -1,11 +1,25 @@
 package com.project.coffee.shop.dao.exception;
 
+/**
+ * Exception for problem with database such as no database created and etc.
+ */
 public class ProblemWithDatabaseException extends DAOException {
 
+    /**
+     * Constructs exception with message.
+     *
+     * @param message exception's message
+     */
     public ProblemWithDatabaseException(String message) {
         super(message);
     }
 
+    /**
+     * Constructs exception with message and cause.
+     *
+     * @param message exception's message
+     * @param cause cause of exception
+     */
     public ProblemWithDatabaseException(String message, Throwable cause) {
         super(message, cause);
     }
